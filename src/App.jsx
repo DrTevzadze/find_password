c3bd3997012794816719b565f9f8894024f28c24
DrTevzadze <dr.davidtevzadze@gmail.com>
2023-11-08 15:37:28 +0400

    Add input button with onChange of the text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import PasswordSection from "./components/PasswordSection";
 import RequirementSection from "./components/RequirementSection";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+
   return (
     <div className="grid-container">
       <div className="left-section">
-        <PasswordSection />
+        <PasswordSection
+          header="Can you find the password?"
+          text={text}
+          change={(e) => setText(e.target.value)}
+        />
       </div>
       <div className="right-section">
         <RequirementSection />
--- src/components/PasswordSection.jsx
+++ src/components/PasswordSection.jsx
@@ -1,7 +1,11 @@
-function PasswordSection() {
+/* eslint-disable react/prop-types */
+
+function PasswordSection({ header, text, change }) {
   return (
     <div className="pass-section">
-      <h1>PASSWORD SECTION</h1>
+      <h1>{header}</h1>
+      <input type="text" onChange={change} />
+      <h2>{text}</h2>
     </div>
   );
 }
