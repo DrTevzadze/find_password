e6ebd808fb2e82581ba4c8d7e15c2dd29db943d2
DrTevzadze <dr.davidtevzadze@gmail.com>
2023-11-09 16:07:36 +0400

    Correctly setup vitest for the future testing purposes

---
+++ src/components/utilities/test.test.js
@@ -0,0 +1,17 @@
+import { expect, test } from "vitest";
+import {
+  includeLowerCase,
+  includeUpperCase,
+  includeSpecialCharacters,
+  includeNumber,
+  sumOfNumbers,
+  spellLastnameBackwards,
+  rainbowColor,
+  palindrome,
+  fifthAndSeventhNumber,
+  symbolEnd,
+} from "./utilities";
+
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(sumNumbers(3)).toEqual(3);
+// });
