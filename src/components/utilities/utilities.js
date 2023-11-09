2ca91685ddfa5f547e4300f1eab1a7bcd0051b93
DrTevzadze <dr.davidtevzadze@gmail.com>
2023-11-09 15:41:50 +0400

    add utilities dir with future requirement functions (functions declared, not finished)

---
+++ src/components/utilities/utilities.js
@@ -0,0 +1,51 @@
+// Level 1
+
+const includeLowerCase = () => {};
+
+// Level 2
+
+const includeUpperCase = () => {};
+// Level 3
+
+const includeSpecialCharacters = () => {};
+
+// Level 4
+
+const includeNumber = () => {};
+
+// Level 5
+
+const sumOfNumbers = () => {};
+
+// Level 6
+
+const spellLastnameBackwards = () => {};
+
+// Level 7 - Include one of the colors of the rainbow
+
+const rainbowColor = () => {};
+
+// Level 8 - Last 5 text letters need to be palindrome ("ana" = "ana"; "Ana" != "ana")
+
+const palindrome = () => {};
+
+// Level 9 - The 5th and 7th characters must be numbers.
+
+const fifthAndSeventhNumber = () => {};
+
+// Level 10 - Place a random special character at the end of the password.
+
+const symbolEnd = () => {};
+
+export {
+  includeLowerCase,
+  includeUpperCase,
+  includeSpecialCharacters,
+  includeNumber,
+  sumOfNumbers,
+  spellLastnameBackwards,
+  rainbowColor,
+  palindrome,
+  fifthAndSeventhNumber,
+  symbolEnd,
+};
