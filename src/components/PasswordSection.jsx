3d51937177301b204b873581c1dba127b67c494c
DrTevzadze <dr.davidtevzadze@gmail.com>
2023-11-13 16:58:50 +0400

    change the password length text and location

--- src/components/PasswordSection.jsx
+++ src/components/PasswordSection.jsx
@@ -9,16 +9,19 @@ function PasswordSection({ text, change }) {
   }, []);
 
   return (
-    <>
+    <div>
       <header>
         <h1>The Password Game</h1>
       </header>
       <div className="pass-section">
         <label htmlFor="passwordInput"></label>
+        <div className="choose-pass">
+          <h3>Please choose a pasword: </h3>
+          <h3>{text ? text.length : 0}</h3>
+        </div>
         <textarea ref={contentEditable} type="text" onChange={change} />
-        <h2>Length: {text ? text.length : 0}</h2>
       </div>
-    </>
+    </div>
   );
 }
 
